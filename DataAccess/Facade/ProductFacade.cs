1197deb414d2492232136eb84be0387843c9ab28
lucianohorvath <lucianohorvath94@gmail.com>
2020-06-07 19:50:42 -0300

    add product facade and sql implementation

---
+++ DataAccess/Facade/ProductFacade.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Facade
+{
+    public class ProductFacade : IFacadeCRUD<Product>
+    {
+        private static ProductFacade instance = new ProductFacade();
+        private static ProductDAOSql implementation = new ProductDAOSql(); // todo: esto podría ser un factory
+
+        /// <summary>
+        /// Private constructor (singleton).
+        /// </summary>
+        private ProductFacade() { }
+
+        /// <summary>
+        /// Gets the singleton instance.
+        /// </summary>
+        public static ProductFacade GetInstance()
+        {
+            return instance;
+        }
+
+        public void Create(Product p)
+        {
+            implementation.Create(p);
+        }
+
+        public void Update(Product p)
+        {
+            implementation.Update(p);
+        }
+
+        public void Delete(Product p)
+        {
+            implementation.Delete(p);
+        }
+
+        public Product Get(int id)
+        {
+            return implementation.Get(id);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return implementation.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Implementations/ProductDAOSql.cs
@@ -0,0 +1,56 @@
+﻿using Common.Entities;
+using DataAccess.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Implementations
+{
+    internal class ProductDAOSql : IProductDAO
+    {
+        public void Create(Product p)
+        {
+            using (lppaDbContext db = new lppaDbContext())
+            {
+                db.Product.Add(p);
+                db.SaveChanges();
+            }
+        }
+
+        public void Update(Product p)
+        {
+            using (lppaDbContext db = new lppaDbContext())
+            {
+                db.Entry(p).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(Product p)
+        {
+            using (lppaDbContext db = new lppaDbContext())
+            {
+                db.Product.Remove(p);
+                db.SaveChanges();
+            }
+        }
+
+        public Product Get(int id)
+        {
+            using (lppaDbContext db = new lppaDbContext())
+            {
+                return db.Product.Find(id);
+            }
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            using (lppaDbContext db = new lppaDbContext())
+            {
+                return db.Product;
+            }
+        }
+    }
+}
