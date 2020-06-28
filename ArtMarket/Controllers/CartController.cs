da458ef2c0b19d89cdfd24102ff9ee4e8e42c9a0
Alexis <alexisrk5@gmail.com>
2020-06-28 14:59:37 -0300

    .- Hard refactor over main project to include user features.

---
+++ ArtMarket/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComprasOnline.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            //Get All Cart Items
+            return View();
+        }
+
+        public ActionResult Add(int id)
+        {
+            //Add to cart
+            return Index();
+        }
+    }
+}
